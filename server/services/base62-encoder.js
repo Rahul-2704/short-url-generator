985a8fe378974211500e52f2490f753ae6a62664
Rahul-2704 <rahulruke35@gmail.com>
2025-07-13 23:53:08 +0530

    initial commit

---
+++ server/services/base62-encoder.js
@@ -0,0 +1,11 @@
+function encodeBase62(number){
+    let allowed_chars='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let encoded_str='';
+
+    while(number>0){
+        encoded_str=chars[number%62]+encoded_str
+        number=Math.floor(number/62);
+    }
+
+    return encoded_str || '0';
+}
